0f1ae35a43d603e3258dcf9fb51c3e88a7f3c7d0
satyandrab <satyandrab@gmail.com>
2014-10-17 05:35:16 +0000

    updated

--- xpath_tester/nutrahealthsupply.php
+++ xpath_tester/nutrahealthsupply.php
@@ -1,37 +1,14 @@
-http://www.nutrahealthsupply.com/GAT/Nitraflex
+<?php
+$test_url = 'http://www.nutrahealthsupply.com/GAT/Nitraflex';
+$arrXpaths = array(
+					'Price' => '//h2[@id="productPrices"]/text()',
+					'Product Name' => '//h1[@id="productName"]/text()',
+					'UPC' => '',
+					'SKU' => '',
+					'Image' => '//div[@id="productMainImage"]/noscript/text()',
+					'Retail Price' => '',
+				  );
 
-Price
+require('includes/main.php');
+?>
 
-//h2[@id='productPrices']/text()
-
-DOMNodeList Object
-(
-    [length] => 1
-)
-$29.99
-
-Product Name
-
-//h1[@id='productName']/text()
-
-DOMNodeList Object
-(
-    [length] => 1
-)
-GAT NitraFlex 30 Servings - Best Sale Price
-
-UPC
-
-
-SKU
-
-Image
-//div[@id='productMainImage']/noscript
-
-DOMNodeList Object
-(
-    [length] => 1
-)
-<a href="http://www.nutrahealthsupply.com/index.php?main_page=popup_image&amp;pID=333" target="_blank"><img src="bmz_cache/4/4abf96989df745f1d6112aaca9a10464.image.300x300.jpg" alt="GAT NitraFlex 30 Servings - Best Sale Price" title=" GAT NitraFlex 30 Servings - Best Sale Price " width="300" height="300" /><br /><span class="imgLink">+ zoom</span></a>
-
-Retail Price
